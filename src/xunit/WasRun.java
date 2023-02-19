ee5e42ec6d354973eca5e92d4fd25480c4fb1a49
LeeJaeHoon <dlwogns3413@naver.com>
2023-02-20 01:54:41 +0900

    Refactor: templateMethod pattern 적용

--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -4,21 +4,19 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class WasRun extends TestCase {
-    public boolean wasRun;
-    public boolean wasSetUp;
+    public String log;
 
     @Override
     public void setUp() {
-        wasSetUp = true;
+        log = "setUp";
     }
 
     public WasRun(String name) {
         super(name);
-
     }
 
     public void testMethod() {
-        wasRun = true;
+        log += " testMethod";
     }
 
 }
