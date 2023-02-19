2ff5d284b7039eb853d0e0b9eb866fa78178d103
LeeJaeHoon <dlwogns3413@naver.com>
2023-02-20 01:37:32 +0900

    Refactor: TestCaseTest setUp Method

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -5,15 +5,20 @@ public class TestCaseTest extends TestCase {
         super(name);
     }
 
+    WasRun wasRun;
+
+    @Override
+    public void setUp() {
+        wasRun = new WasRun("testMethod"); // 생성자로 테스트할 메소드 지정
+    }
+
     public void testRunning() {
-        WasRun wasRun = new WasRun("testMethod"); // 생성자로 테스트할 메소드 지정
         Assert.assertEquals(false,wasRun.wasRun);
         wasRun.run(); // 메소드 실행시 wasRun true로
         Assert.assertEquals(true,wasRun.wasRun);
     }
 
     public void testSetUp() {
-        WasRun wasRun = new WasRun("testMethod"); // 생성자로 테스트할 메소드 지정
         Assert.assertEquals(false,wasRun.wasSetUp);
         wasRun.run(); // 메소드 실행시 wasSetUp true로
         Assert.assertEquals(true,wasRun.wasSetUp);
