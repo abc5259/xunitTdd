86b610df04cec96fc1b6dd0770ab9e777e31ca07
LeeJaeHoon <dlwogns3413@naver.com>
2023-02-20 01:35:33 +0900

    Feat: TestCase setUp Method

--- src/xunit/TestCase.java
+++ src/xunit/TestCase.java
@@ -10,11 +10,16 @@ public class TestCase {
     }
 
     public void run() { //생성자로 설정한 메소드를 실행해주는 역할
+        setUp();
+
         try {
             Method method = getClass().getMethod(name);
             method.invoke(this);
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void setUp() {
     }
 }
--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -11,4 +11,11 @@ public class TestCaseTest extends TestCase {
         wasRun.run(); // 메소드 실행시 wasRun true로
         Assert.assertEquals(true,wasRun.wasRun);
     }
+
+    public void testSetUp() {
+        WasRun wasRun = new WasRun("testMethod"); // 생성자로 테스트할 메소드 지정
+        Assert.assertEquals(false,wasRun.wasSetUp);
+        wasRun.run(); // 메소드 실행시 wasSetUp true로
+        Assert.assertEquals(true,wasRun.wasSetUp);
+    }
 }
